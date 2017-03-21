7adb97e048b466fc1ac313214da5898e34786c62
Yolanda Zhou <yolandaz@mit.edu>
2017-03-21 02:31:37 -0400

    scene images

---
+++ Assets/Scripts/LoadStart2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadStart2 : MonoBehaviour {
+
+	// Use this for initialization
+	void LoadDifficultyScreen () {
+		Application.LoadLevel ("Main");
+	}
+}
