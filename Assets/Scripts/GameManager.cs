c728272c4e991575909895eeb41a4286732266ef
Budmonde Duinkharjav <budmonde@gmail.com>
2017-03-20 23:11:48 -0400

    Create Game Over screen and UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public static GameManager instance = null;
     public BoardManager boardScript;
+    public float maxTime = 300f;
+    public int lives = 5;
+    public float time;
     public int doggos;
-    public int unhappyPetCounter = 0;
+
+    private Text lifeText;
+    private Text levelText;
+    private GameObject levelImage;
+    public float levelStartDelay = 2f;
 
     void Awake() {
         if (instance == null) {
@@ -21,14 +29,44 @@ public class GameManager : MonoBehaviour {
     }
 	
     void InitGame() {
+        lifeText = GameObject.Find("LifeText").GetComponent<Text>();
+        lifeText.text = "Lives: " + lives;
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Start Game";
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+
+        time = 0;
         boardScript.SetupScene(doggos);
     }
 
+    void HideLevelImage() {
+        levelImage.SetActive(false);
+    }
+
+    void Update() {
+        time += Time.deltaTime;
+        if (isGameOver()) {
+            Invoke("GameOver", levelStartDelay);
+        }
+    }
+
 	public void incrementUnhappyPetCounter() {
-		unhappyPetCounter++;
+		lives--;
+        lifeText.text = "Lives: " + lives;
 	}
 
-	private bool checkEndGame() {
-		return unhappyPetCounter > 5;
+	private bool isGameOver() {
+        if (time >= maxTime || lives <= 0) {
+            return true;
+        } else {
+            return false;
+        }
 	}
+    private void GameOver() {
+        levelText.text = "Game Over";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
 }
